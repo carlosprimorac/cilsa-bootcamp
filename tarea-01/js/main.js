f0d6b04c08d4f3605048031f1b43c28a6a091e00
Carlos R. Primorac <carlosprimorac@gmail.com>
2024-08-14 17:00:07 -0300

    add validar campos de texto

--- tarea-01/js/main.js
+++ tarea-01/js/main.js
@@ -10,9 +10,27 @@ const linkElement = document.querySelector('link');
 const toogleStyleButton = document.querySelector('#toogleStyle');
 const personalDataForm = document.querySelector('#personal-data-form');
 const birthdate = document.querySelector('#birthdate');
+const firstName = document.querySelector('#first-name');
+const lastName = document.querySelector('#last-name');
+const country = document.querySelector('#country');
 
 // functions 
 const getAge = (birthdate) => Math.floor((new Date() - new Date(birthdate).getTime()) / 3.15576e+10)
+
+const validateNameForField = (name, field) => {
+    let regexp = /^[A-Za-zÁÉÍÓÚáéíóúÑñÜü][A-Za-zÁÉÍÓÚáéíóúÑñÜü\s'-]{2,50}$/;
+    if (!regexp.test(name)) {
+        alert(`
+            El ${field} puede contener únicamente de 2 a 50 
+            caracteres (sin espacios en blanco al inicio), 
+            incluyendo letras (mayúsculas, minúsculas con o 
+            sin tilde) y espacios`);
+        return false;
+    }
+    return true;
+}
+
+
 
 // events
 toogleStyleButton.innerText = 'Alternar estilo de alto contraste';
@@ -38,6 +56,18 @@ personalDataForm.addEventListener('submit', (event) => {
     if (getAge(birthdate.value) < LEGAL_AGE) {
         alert('Tienes que ser mayor de 18 años para registrar tus datos');
         return false;
+    }
+
+    if (!validateNameForField(firstName.value, 'nombre')) {
+        return;
+    }
+
+    if (!validateNameForField(lastName.value, 'apellido')) {
+        return;
+    }
+
+    if (!validateNameForField(country.value, 'país')) {
+        return;
     }
 
     alert('Se enviaron tus datos');
